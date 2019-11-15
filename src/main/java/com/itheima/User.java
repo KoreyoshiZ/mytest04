8368f95ded4d5265b278a162dcd6827e31e26d6e
zzc <903117921@qq.com>
2019-11-15 11:23:24 +0800

    提交本地仓库

---
+++ src/main/java/com/itheima/User.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @ProjectName helloworld
+ * @Package com.itheima
+ * @ClassName: User
+ * @Author ZZC
+ * @CreateDate 2019/11/15 11:22
+ * @Description: 描述:
+ */
+public class User {
+
+}
